6d3f1d6d2e5e7c2262a47ef83c38807fac8b0334
Nathan8044 <87392271+Nathan8044@users.noreply.github.com>
2023-04-12 21:20:50 -0400

    Create complex_commands.py

---
+++ complex_commands.py
@@ -0,0 +1,13 @@
+from netmiko import ConnectHandler 
+
+#create a dcitionary with all the contents for the name 
+
+
+#send the config commands 
+
+#output the config commands 
+
+
+
+#copy to text file use try, except and return. 
+
